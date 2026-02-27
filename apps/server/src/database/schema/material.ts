e1bb428fd5207c57b5c7174e7bafdb32e9e4f290
rizqyn9 <rizqynugroho9@gmail.com>
2026-02-27 20:17:47 +0700

    feat: server - material schema

---
+++ apps/server/src/database/schema/material.ts
@@ -0,0 +1,47 @@
+import { boolean, integer, numeric, pgTable, primaryKey, serial, varchar } from 'drizzle-orm/pg-core'
+
+import { metafields } from '@/database/schema/common'
+
+export const masterialCategoryTable = pgTable('masterialCategories', {
+  id: serial().primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  description: varchar({ length: 255 }),
+  ...metafields,
+})
+
+export const uomTable = pgTable('uoms', {
+  code: varchar({ length: 255 }).notNull().primaryKey(),
+  ...metafields,
+})
+
+export const materialTable = pgTable('masterials', {
+  id: serial().primaryKey(),
+  name: varchar({ length: 255 }).notNull().unique(),
+  description: varchar({ length: 255 }),
+  sku: varchar({ length: 255 }).notNull().unique(),
+  categoryId: integer()
+    .notNull()
+    .references(() => masterialCategoryTable.id),
+  baseUomId: integer()
+    .notNull()
+    .references(() => uomTable.code),
+  isActive: boolean().default(true).notNull(),
+  ...metafields,
+})
+
+export const materialUomConversionTable = pgTable(
+  'materialUomConversions',
+  {
+    materialId: integer()
+      .notNull()
+      .references(() => materialTable.id),
+    fromUomCode: integer()
+      .notNull()
+      .references(() => uomTable.code),
+    toUomCode: integer()
+      .notNull()
+      .references(() => uomTable.code),
+    multiplier: numeric().notNull(),
+  },
+  (t) => [primaryKey({ columns: [t.materialId, t.fromUomCode, t.toUomCode] })]
+)
