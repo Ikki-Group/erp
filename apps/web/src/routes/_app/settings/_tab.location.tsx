bd8bb870b7964a6d94b51fe53822f8cf61f2aecf
rizqyn9 <rizqynugroho9@gmail.com>
2026-03-03 06:33:19 +0700

    feat: metadata stamp and auth

--- apps/web/src/routes/_app/settings/_tab.location.tsx
+++ apps/web/src/routes/_app/settings/_tab.location.tsx
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 import { Link, createFileRoute } from '@tanstack/react-router'
 import { createColumnHelper } from '@tanstack/react-table'
 import { PencilIcon } from 'lucide-react'
-import type { LocationDto } from '@/features/location';
+import type { LocationDto } from '@/features/location'
 import { DataTableCard } from '@/components/card/data-table-card'
 import { BadgeDot, getActiveStatusBadge } from '@/components/common/badge-dot'
 import { Button } from '@/components/ui/button'
@@ -25,13 +25,13 @@ const columns = [
     id: 'action',
     cell: ({ row }) => {
       return (
-        <div className="flex items-center justify-center">
+        <div className='flex items-center justify-center'>
           <Button
-            variant="outline"
-            size="icon-sm"
+            variant='outline'
+            size='icon-sm'
             render={
               <Link
-                to="/settings/location/$id"
+                to='/settings/location/$id'
                 params={{ id: String(row.original.id) }}
               />
             }
@@ -47,9 +47,9 @@ const columns = [
   ch.accessor('name', {
     header: 'Lokasi',
     cell: ({ row }) => (
-      <div className="flex gap-2 flex-col">
+      <div className='flex gap-2 flex-col'>
         <p>{row.original.name}</p>
-        <p className="text-muted-foreground text-xs italic">
+        <p className='text-muted-foreground text-xs italic'>
           ({row.original.code})
         </p>
       </div>
@@ -67,7 +67,7 @@ const columns = [
   }),
   ch.accessor('description', {
     header: 'Deskripsi',
-    cell: ({ row }) => row.original.description ?? '-',
+    cell: ({ row }) => row.original.description || '-',
     enableSorting: false,
     size: 200,
   }),
@@ -83,7 +83,7 @@ function LocationsTable() {
   const { data, isLoading } = useQuery(
     locationApi.list.query({
       ...ds.pagination,
-    }),
+    })
   )
 
   const table = useDataTable({
@@ -96,14 +96,14 @@ function LocationsTable() {
 
   return (
     <DataTableCard
-      title="Daftar Lokasi"
+      title='Daftar Lokasi'
       table={table}
       isLoading={isLoading}
       recordCount={data?.meta.total || 0}
       action={
         <Button
-          size="sm"
-          render={<Link from={Route.fullPath} to="/settings/location/create" />}
+          size='sm'
+          render={<Link from={Route.fullPath} to='/settings/location/create' />}
           nativeButton={false}
         >
           Tambah Lokasi
