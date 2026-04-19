ad4b9747bfcd5b94fe46826e4f69a7c408cbafe4
rizqyn9 <rizqynugroho9@gmail.com>
2026-04-19 23:09:30 +0700

    refactor: remove unused cache configuration comments and linting overrides

--- apps/server/src/core/cache.ts
+++ apps/server/src/core/cache.ts
@@ -1,4 +1,3 @@
-// oxlint-disable typescript/no-unsafe-assignment
 import { BentoCache, bentostore } from 'bentocache'
 import { memoryDriver } from 'bentocache/drivers/memory'
 import { createCache } from 'cache-manager'
@@ -16,40 +15,5 @@ export const bento = new BentoCache({
 	},
 })
 
-// await bento.set({ key: 'user:42', value: { name: 'jul' } })
-// console.log(await bento.get({ key: 'user:42' }))
-
-// let counter = 0
-// try {
-// 	const res = await bento.getOrSet({
-// 		key: 'example',
-// 		factory: ({ fail, skip }) => {
-// 			console.log('Hit', ++counter)
-
-// 			return skip()
-// 			return 'faa'
-// 		},
-// 	})
-
-// 	console.log({
-// 		res,
-// 		next: await bento.get({ key: 'example' }),
-// 		next2: await bento.get({ key: 'example' }),
-// 		next3: await bento.get({ key: 'example' }),
-// 	})
-
-// 	await sleep(1000)
-// 	console.log({
-// 		next: await bento.get({ key: 'example' }),
-// 		next2: await bento.get({ key: 'example' }),
-// 		next3: await bento.get({ key: 'example' }),
-// 	})
-// } catch (error) {
-// 	console.error({
-// 		isErr: error instanceof NotFoundError,
-// 	})
-// }
-
-// TODO
 // Handle serialization/deserialization of cache values
 export const cache = createCache({})
