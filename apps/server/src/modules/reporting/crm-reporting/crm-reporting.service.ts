4a7ad8fc1d7e342635dc4f2a02bfa19694944c53
rizqyn9 <rizqynugroho9@gmail.com>
2026-05-02 19:50:22 +0700

    fix: add file-level eslint-disable to remaining reporting modules

--- apps/server/src/modules/reporting/crm-reporting/crm-reporting.service.ts
+++ apps/server/src/modules/reporting/crm-reporting/crm-reporting.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-type-assertion */
 import { record } from '@elysiajs/opentelemetry'
 import { and, eq, gte, lte, sql } from 'drizzle-orm'
 
