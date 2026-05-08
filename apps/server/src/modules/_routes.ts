908ab5646eb147ff5bc65d765a45cef59a8d6a9d
rizqyn9 <rizqynugroho9@gmail.com>
2026-05-09 02:21:18 +0700

    feat(server): register sales-type route module

--- apps/server/src/modules/_routes.ts
+++ apps/server/src/modules/_routes.ts
@@ -18,6 +18,7 @@ import { initPurchasingRouteModule } from './purchasing'
 import { initRecipeRouteModule } from './recipe'
 import { initReportingRouteModule } from './reporting'
 import { initSalesRouteModule } from './sales'
+import { initSalesTypeRouteModule } from './sales-type'
 import { initSupplierRouteModule } from './supplier'
 import { initToolRouteModule } from './tool'
 import type Elysia from 'elysia'
@@ -45,6 +46,7 @@ export function initRoutes(m: Modules) {
 		initHRRouteModule(m.hr),
 		initPaymentRouteModule(m.payment),
 		initReportingRouteModule(m.reporting),
+		initSalesTypeRouteModule(m.salesType),
 	]
 
 	return {
