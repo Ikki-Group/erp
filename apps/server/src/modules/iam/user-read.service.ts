2547c6d351bc24f2d99c6302efab902b3e6a2e5a
rizqyn9 <rizqynugroho9@gmail.com>
2026-05-17 08:47:39 +0700

    refactor(iam): update user read service to improve relation loading and mapping

--- apps/server/src/modules/iam/user-read.service.ts
+++ apps/server/src/modules/iam/user-read.service.ts
@@ -2,12 +2,13 @@ import { record } from '@elysiajs/opentelemetry'
 
 import type { WithPaginationResult } from '@/core/database'
 
-import type { UserAssignmentService } from '@/modules/iam/assignment.service'
-import type { RoleService } from '@/modules/iam/role.service'
-import type { UserReadDetailSchema } from '@/modules/iam/user-read.schema'
-import type { UserFilterSchema } from '@/modules/iam/user.schema'
-import type { UserService } from '@/modules/iam/user.service'
 import type { LocationServiceModule } from '@/modules/location'
+
+import type { UserAssignmentService } from './assignment.service'
+import type { RoleService } from './role.service'
+import type { UserReadDetailSchema } from './user-read.schema'
+import type { UserFilterSchema, UserSchema } from './user.schema'
+import type { UserService } from './user.service'
 
 interface Deps {
 	svc: {
@@ -30,13 +31,13 @@ interface UserRelations {
 export class UserReadService {
 	constructor(private readonly deps: Deps) {}
 
-	async loadUserRelations(svc: Deps['svc'], userIds: number[]): Promise<UserRelations> {
+	async #loadUserRelations(userIds: number[]): Promise<UserRelations> {
 		const [assignments, roleMap, superadmin, locationMap, locations] = await Promise.all([
-			svc.assignment.getListByUserIds(userIds),
-			svc.role.getRelationMap(),
-			svc.role.getSuperadmin(),
-			svc.location.getRelationMap(),
-			svc.location.getListAll(),
+			this.deps.svc.assignment.getListByUserIds(userIds),
+			this.deps.svc.role.getRelationMap(),
+			this.deps.svc.role.getSuperadmin(),
+			this.deps.svc.location.getRelationMap(),
+			this.deps.svc.location.getListAll(),
 		])
 
 		return {
@@ -47,68 +48,46 @@ export class UserReadService {
 			locations,
 		}
 	}
-	// oxlint-disable-next-line no-unused-private-class-members typescript/require-await no-unused-private-class-members
-	// async #buildUserAssignments(
-	// 	user: UserSchema,
-	// 	roleMapper?: RelationMap<number, RoleSchema>,
-	// 	locationMapper?: RelationMap<number, LocationSchema>,
-	// ) {
-	// 	// oxlint-disable-next-line typescript/no-unsafe-type-assertion
-	// 	const result: UserReadDetailSchema = { ...user } as UserReadDetailSchema
-	// 	return result
-	// }
+
+	#mapUserDetail(rawUser: UserSchema, relations: UserRelations): UserReadDetailSchema {
+		const user: UserReadDetailSchema = {
+			...rawUser,
+			assignments: [],
+		}
+
+		if (user.isRoot) {
+			user.assignments = relations.locations.map((location) => ({
+				...this.deps.svc.assignment.getDefaultAssignmentForSuperadmin(),
+				location,
+				role: relations.superadmin,
+			}))
+		} else {
+			const uas = relations.assignments[user.id]
+			if (uas && uas.length > 0) {
+				user.assignments = uas.map((ua) => ({
+					...ua,
+					role: relations.roleMap.getRequired(ua.roleId),
+					location: relations.locationMap.getRequired(ua.locationId),
+				}))
+			}
+		}
+
+		return user
+	}
+
+	async #enrichUsers(rawUsers: UserSchema[]): Promise<UserReadDetailSchema[]> {
+		if (rawUsers.length === 0) return []
+		const userIds = Array.from(new Set(rawUsers.map((u) => u.id)))
+		const relations = await this.#loadUserRelations(userIds)
+		return rawUsers.map((u) => this.#mapUserDetail(u, relations))
+	}
 
 	/* --------------------------------- HANDLER -------------------------------- */
 
 	async handleList(filter: UserFilterSchema): Promise<WithPaginationResult<UserReadDetailSchema>> {
 		return record('UserReadService.handleList', async () => {
 			const { data, meta } = await this.deps.svc.user.getListPaginated(filter)
-			const userIds = Array.from(new Set(data.map((u) => u.id)))
-
-			const [assignments, roleMap, superadmin, locationMap, locations] = await Promise.all([
-				this.deps.svc.assignment.getListByUserIds(userIds),
-				this.deps.svc.role.getRelationMap(),
-				this.deps.svc.role.getSuperadmin(),
-				this.deps.svc.location.getRelationMap(),
-				this.deps.svc.location.getListAll(),
-			])
-
-			// Precompute superadmin assignments ONCE
-			const superadminAssignments = locations.map((loc) => ({
-				...this.deps.svc.assignment.getDefaultAssignmentForSuperadmin(),
-				location: loc,
-				role: superadmin,
-			}))
-
-			const users: UserReadDetailSchema[] = []
-
-			for (const rawUser of data) {
-				const user: UserReadDetailSchema = {
-					...rawUser,
-					assignments: [],
-				}
-
-				if (user.isRoot) {
-					user.assignments = superadminAssignments
-				} else {
-					const uas = assignments[user.id]
-					if (uas && uas.length > 0) {
-						for (const ua of uas) {
-							user.assignments = [
-								...user.assignments,
-								{
-									...ua,
-									role: roleMap.getRequired(ua.roleId),
-									location: locationMap.getRequired(ua.locationId),
-								},
-							]
-						}
-					}
-				}
-
-				users.push(user)
-			}
-
+			const users = await this.#enrichUsers(data)
 			return { meta, data: users }
 		})
 	}
