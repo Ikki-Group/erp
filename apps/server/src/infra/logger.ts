186cd16541f3de82699b16626260f143b18c26cc
rizqyn9 <rizqynugroho9@gmail.com>
2026-05-17 14:35:33 +0700

    fix: restructure

---
+++ apps/server/src/infra/logger.ts
@@ -0,0 +1,46 @@
+import {
+	configure,
+	getLogger as getLogtapeLogger,
+	getConsoleSink,
+	type Logger,
+	jsonLinesFormatter,
+} from '@logtape/logtape'
+import { getPrettyFormatter } from '@logtape/pretty'
+
+import { env } from '@/config/env'
+
+await configure({
+	sinks: {
+		console: getConsoleSink(),
+		main: getConsoleSink({
+			formatter:
+				env.LOG_FORMAT === 'pretty'
+					? getPrettyFormatter({
+							icons: false,
+							timestamp: 'time',
+							properties: true,
+							align: false,
+							messageColor: 'cyan',
+							messageStyle: 'reset',
+						})
+					: jsonLinesFormatter,
+		}),
+		// otel: getOpenTelemetrySink({
+		// 	diagnostics: true,
+		// 	loggerProvider,
+		// }),
+	},
+	loggers: [
+		{ category: ['logtape', 'meta'], sinks: ['console'], lowestLevel: 'error' },
+		// { category: ['otel'], sinks: ['otel'], lowestLevel: 'debug' },
+		{ category: [], sinks: ['main'], lowestLevel: 'debug' },
+	],
+})
+
+export function getLogger(category: string[] = []): Logger {
+	return getLogtapeLogger([...category])
+}
+
+const logger = getLogtapeLogger([])
+
+export { logger }
