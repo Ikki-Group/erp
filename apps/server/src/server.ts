53e93215dd939f7ee72b7d0c4bdb80f39d35dc84
rizqyn9 <rizqynugroho9@gmail.com>
2026-06-03 20:58:40 +0700

    fix(server): reorganize imports and enhance app startup logging

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,28 +1,31 @@
 import { db } from '@/db'
 
 import { env } from '@/config/env'
+import { createCache } from '@/infra/cache'
+import { logger } from '@/infra/logger'
 
 import { createModules } from '@/modules/_registry'
 import { createRoutes } from '@/modules/_routes'
 
 import { createApp } from './app'
-import { createCache } from '@/infra/cache'
-import { logger } from '@/infra/logger'
 
 const cacheClient = createCache()
 const modules = createModules(db, cacheClient)
 const routes = createRoutes(modules)
 
 const app = createApp(modules)
+
 routes.register(app)
 
-app.listen({ port: env.PORT })
-
-logger.info(`${env.APP_NAME} is running at http://${env.HOST}:${env.PORT}`, {
-	port: env.PORT,
-	host: env.HOST,
-	env: env.NODE_ENV,
-})
+app
+	.onStart(() => {
+		logger.info(`${env.APP_NAME} is running at http://${env.HOST}:${env.PORT}`, {
+			port: env.PORT,
+			host: env.HOST,
+			env: env.NODE_ENV,
+		})
+	})
+	.listen({ port: env.PORT, hostname: env.HOST })
 
 async function shutdown() {
 	logger.info('Shutting down')
