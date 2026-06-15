75e57efafd266e2ff4cdb842d2324cb8c2571e89
rizqyn9 <rizqynugroho9@gmail.com>
2026-06-16 05:07:51 +0700

    feat(routes): refactor route imports and streamline route creation for improved clarity

--- apps/server/src/modules/_routes.ts
+++ apps/server/src/modules/_routes.ts
@@ -1,35 +1,35 @@
+import { createIamRoute } from '@/modules/iam/iam.route'
+import { createLocationRoute } from '@/modules/location/location.route'
+
 import type { Modules } from './_registry'
-import { createIamRoute } from './iam/iam.route'
-import { initInventoryRouteModule } from './inventory'
-import { createLocationRouteModule } from './location'
-import { initMaterialRoutes } from './material'
-import { initToolRouteModule } from './tool'
 import type Elysia from 'elysia'
 
 export function createRoutes(m: Modules) {
-	const routes = [
-		createLocationRouteModule(m.location),
-		createIamRoute(m.iam),
-		// createAuthRouteModule(m.auth),
-		// initDashboardRouteModule(m.dashboard),
-		initInventoryRouteModule(m.inventory),
-		initMaterialRoutes(m.material),
-		// initProductRouteModule(m.product),
-		// initRecipeRouteModule(m.recipe),
-		initToolRouteModule(m.tool),
-		// initMokaRouteModule(m.moka),
-		// initSalesRouteModule(m.sales),
-		// initSupplierRouteModule(m.supplier),
-		// initFinanceRouteModule(m.finance),
-		// initCrmRouteModule(m.crm),
-		// initCompanyRouteModule(m.company),
-		// initAuditRouteModule(m.audit),
-		// initPurchasingRouteModule(m.purchasing),
-		// initProductionRouteModule(m.production),
-		// initHRRouteModule(m.hr),
-		// initPaymentRouteModule(m.payment),
-		// initReportingRouteModule(m.reporting),
-	]
+	// const routes = [
+	// 	createLocationRouteModule(m.location),
+	// 	createIamRoute(m.iam),
+	// 	// createAuthRouteModule(m.auth),
+	// 	// initDashboardRouteModule(m.dashboard),
+	// 	initInventoryRouteModule(m.inventory),
+	// 	initMaterialRoutes(m.material),
+	// 	// initProductRouteModule(m.product),
+	// 	// initRecipeRouteModule(m.recipe),
+	// 	// initToolRouteModule(m.tool),
+	// 	// initMokaRouteModule(m.moka),
+	// 	// initSalesRouteModule(m.sales),
+	// 	// initSupplierRouteModule(m.supplier),
+	// 	// initFinanceRouteModule(m.finance),
+	// 	// initCrmRouteModule(m.crm),
+	// 	// initCompanyRouteModule(m.company),
+	// 	// initAuditRouteModule(m.audit),
+	// 	// initPurchasingRouteModule(m.purchasing),
+	// 	// initProductionRouteModule(m.production),
+	// 	// initHRRouteModule(m.hr),
+	// 	// initPaymentRouteModule(m.payment),
+	// 	// initReportingRouteModule(m.reporting),
+	// ]
+
+	const routes = [createLocationRoute(m.location), createIamRoute(m.iam)]
 
 	return {
 		register: (app: Elysia): Elysia => {
